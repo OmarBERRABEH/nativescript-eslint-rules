46f682a352a493ca5007cb4a765c001176a944a8
sis0k0 <stanimira.vlaeva@gmail.com>
2019-09-09 19:45:35 +0300

    test(no-short-imports): add unit test cases

---
+++ test/no-short-imports.spec.ts
@@ -0,0 +1,63 @@
+import { RuleTester, Rule } from "eslint";
+
+import rule from "../src/rules/no-short-imports";
+
+const ruleTester = new RuleTester({
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+    },
+});
+
+ruleTester.run("no-short-imports", rule as Rule.RuleModule, {
+    invalid: [
+        getInvalidTest(
+            "import { SearchBar } from \"ui/search-bar\"",
+            "import { SearchBar } from \"tns-core-modules/ui/search-bar\"",
+        ),
+        getInvalidTest(
+            "import { SearchBar } from 'ui/search-bar'",
+            "import { SearchBar } from 'tns-core-modules/ui/search-bar'",
+        ),
+        getInvalidTest(
+            "import * as searchBarModule from \"ui/search-bar\"",
+            "import * as searchBarModule from \"tns-core-modules/ui/search-bar\"",
+        ),
+        getInvalidTest(
+            "import SearchBarExports from \"ui/search-bar\"",
+            "import SearchBarExports from \"tns-core-modules/ui/search-bar\"",
+        ),
+        getInvalidTest(
+            "const searchBarModule = require(\"ui/search-bar\")",
+            "const searchBarModule = require(\"tns-core-modules/ui/search-bar\")",
+        ),
+        getInvalidTest(
+            "const searchBarModule = require(\"ui/search-bar\")",
+            "const searchBarModule = require(\"tns-core-modules/ui/search-bar\")",
+        ),
+        getInvalidTest(
+            "const searchBarModule = require('ui/search-bar')",
+            "const searchBarModule = require('tns-core-modules/ui/search-bar')",
+        ),
+        getInvalidTest(
+            "const SearchBar = require(\"ui/search-bar\").SearchBar",
+            "const SearchBar = require(\"tns-core-modules/ui/search-bar\").SearchBar",
+        ),
+    ],
+
+    valid: [
+        "import { SearchBar } from \"tns-core-modules/ui/search-bar\"",
+        "const text = \"import { SearchBar } from 'ui/search-bar'\"",
+        "console.log('ui/search-bar')",
+    ],
+});
+
+function getInvalidTest(code: string, output: string) {
+    const invalidTest = {
+        code,
+        errors: [{ message: "Using short imports from 'tns-core-modules' is not allowed." }],
+        output,
+    };
+
+    return invalidTest;
+}
